11a218f2229392f688c0ec8e70656f42efb30eec
eric04delarosamoya-ux <eric04delarosamoya@gmail.com>
2026-02-25 10:17:29 +0100

    cesarterminado

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const abece="abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 let frase="casa"
 let paso=3
 for (i=0;i<frase.length;i++) {
-	console.log("Voy a buscar la letra: " + frase[i])
 	let posicion=abece.indexOf (frase[i])
-	console.log("Esta en la posicion: " + posicion)
-	let nuevaposicion=posicion+paso
-	let nuevaletra=abece[nuevaposicion]
-	console.log("Su nueva letra es: "+nuevaletra)
+	let nuevaletra=abece[posicion+paso]
+	let nuevafrase=nuevafrase+nuevaletra
 }
+
+console.log(frase + "es: "+nuevafrase)
