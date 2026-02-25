426c8124a1e61d384e27d95b51eacc3451399fc4
eric04delarosamoya-ux <eric04delarosamoya@gmail.com>
2026-02-25 10:18:57 +0100

    cesar arreglado

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const abece="abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 let frase="casa"
 let paso=3
+
+let nuevafrase=""
 for (i=0;i<frase.length;i++) {
 	let posicion=abece.indexOf (frase[i])
 	let nuevaletra=abece[posicion+paso]
